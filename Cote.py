5400250a4f836210b11fbca408e7f56a2ec6a6ba
leelife96 <leelife77@naver.com>
2023-07-09 19:20:45 +0900

    d

--- Cote.py
+++ Cote.py
@@ -10,3 +10,34 @@ n = input()
 s = input()
 print(int(n)*s) //6037 """
 
+"""a,b=map(int,input().split())
+
+c = a**b
+print(c) //6038 """
+
+""" f1,f2=map(float,input().split())
+print(f1**f2)  //6039 """
+
+""" a,b=map(int,input().split())
+print(a//b) //6040 """
+
+""" a = input()
+a = float(a)
+print(format(a,".2f")) //6042 """
+
+""" f1,f2=map(float,input().split())
+print(format(f1/f2,".3f")) //6043 """
+
+""" a,b=map(int,input().split())
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a%b)
+print(format(a/b,".2f")) //6044 """
+
+""" a,b,c=(int,input().split())
+print(a+b+c)
+print(format((a+b+c)/3,".2f")) //6045 """
+
+
