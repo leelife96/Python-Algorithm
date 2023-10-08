e861b1afec7112a13040109c5485c6526c15d7ea
leelife96 <leelife77@naver.com>
2023-10-08 20:06:56 +0900

    das

--- BAEKJOON/BAEKJOON.py
+++ BAEKJOON/BAEKJOON.py
@@ -259,16 +259,3 @@ print(data)
 # for i in word:
 #     print(i)
 
-import sys
-
-N = int(sys.stdin.readline())
-number = 666
-cnt = 0
-
-while True:
-    if "666" in str(number):
-        cnt += 1
-    if cnt == N:
-        break
-    number += 1
-print(number)
---
+++ BAEKJOON/Silver/[1436번] 영화감독 (실버V)/영화감독.py
@@ -0,0 +1,14 @@
+import sys
+
+N = int(sys.stdin.readline())
+num = 666
+cnt = 0
+
+while True:
+    if "666" in str(num):
+        cnt += 1
+    if cnt == N:
+        break
+    num += 1
+print(num)
+
